2cdf23fd66a50232c7c7f69f1439d2ad2b34a226
Hien Luong Tran Hy <hyhien@gmail.com>
2022-11-26 13:29:53 +0000

    Test

--- src/App.js
+++ src/App.js
@@ -9,16 +9,6 @@ function App() {
         <p>
           GitHub Codespaces <span className="heart">♥️</span> React
         </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
       </header>
     </div>
   );
